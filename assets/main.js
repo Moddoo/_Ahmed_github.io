f09da224d50a2864ceab93d3694b55016018f016
Moddoo <zoddoo@yahoo.com>
2020-01-11 14:31:51 -0600

    perfect

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,32 @@
-let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()=+-_}{][\|/.,><?`~"
-
+let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()=+-_}{][\|/.,><?`~0123456789";
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let numbers = "0123456789";
+let symbols = "!@#$%^&*()=+-_}{][\|/.,><?`~";
+// let password = ""
+if (document.getElementById("upper").checked === true) {
+     randompassword();
+}
 function randompassword() {
-        let password = "";
-        for(i=0; i<12; i++) {
-            password += characters[Math.floor(Math.random() * characters.length)]
-        }
-        console.log(password)
-        document.getElementById("text").innerHTML= password;
+    var passGenString = "";
+    if (document.getElementById("upper").checked){
+        passGenString = passGenString + upperCase
+    }
+    if (document.getElementById("lower").checked){
+        passGenString = passGenString + lowerCase
+    }
+    if (document.getElementById("num").checked){
+        passGenString = passGenString + numbers
+    }
+    if (document.getElementById("symbol").checked){
+        passGenString = passGenString + symbols
+    }
+    console.log(passGenString);
+    let password = "";
+    for(i=0; i<12; i++) {
+        password += passGenString[Math.floor(Math.random() * passGenString.length)]
+    }
+    console.log(password)
+    document.getElementById("text").innerHTML= password;
 }
-
-function copypwd() {
-    document.getElementById("text").select();
-    document.getElementById("text").setSelectionRange(0, 99999);
-    document.execCommand("copy");
-}
+    
