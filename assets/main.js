a22700691a112829cc955de0673d05637e6f848e
Moddoo <zoddoo@yahoo.com>
2020-01-20 23:50:47 -0600

    100%

--- assets/main.js
+++ assets/main.js
@@ -3,35 +3,43 @@ let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowerCase = "abcdefghijklmnopqrstuvwxyz";
 let numbers = "0123456789";
 let symbols = "!@#$%^&*()=+-_}{][\|/.,><?`~";
+let x = prompt("please write the length of your password?");
+console.log(typeof x)
 
 
+if ( parseInt(x) <6 || parseInt(x) > 12 || isNaN(x) || !x) {
+    alert("put number between 5 and 12");
+} else {
 function randompassword() {
-    var passGenString = "";
-    if (document.getElementById("upper").checked){
-        passGenString = passGenString + upperCase
-    }
-    if (document.getElementById("lower").checked){
-        passGenString = passGenString + lowerCase
-    }
-    if (document.getElementById("num").checked){
-        passGenString = passGenString + numbers
-    }
-    if (document.getElementById("symbol").checked){
-        passGenString = passGenString + symbols
-    } 
-    if (!document.getElementById("upper").checked &&
-        !document.getElementById("lower").checked &&
-        !document.getElementById("num").checked &&
-        !document.getElementById("symbol").checked) {
-            return;
+        let passGenString = "";
+        if (document.getElementById("upper").checked){
+            passGenString = passGenString + upperCase
         }
-    console.log(passGenString);
-    let password = "";
-    for(i=0; i<12; i++) {
-        password += passGenString[Math.floor(Math.random() * passGenString.length)]
+        if (document.getElementById("lower").checked){
+            passGenString = passGenString + lowerCase
+        }
+        if (document.getElementById("num").checked){
+            passGenString = passGenString + numbers
+        }
+        if (document.getElementById("symbol").checked){
+            passGenString = passGenString + symbols
+        } 
+        if (!document.getElementById("upper").checked &&
+            !document.getElementById("lower").checked &&
+            !document.getElementById("num").checked &&
+            !document.getElementById("symbol").checked) {
+                return;
+            }
+        
+            let password = "";
+         
+            for(i=0; i<parseInt(x); i++) {
+               
+                password += passGenString[Math.floor(Math.random() * passGenString.length)]
+                document.getElementById("text").innerHTML= password;
+        
     }
-    console.log(password)
-    document.getElementById("text").innerHTML= password;
+}
 }
     
 function copypwd() {
